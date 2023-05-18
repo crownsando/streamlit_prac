8994838809deee9242b2e4a3334103c3666fc8ac
unknown <codud9766@khu.ac.kr>
2023-05-18 12:36:47 +0900

    change file names

--- 02_streamlit_example.py
+++ 02_streamlit_example.py
@@ -17,7 +17,7 @@ with col1:
 
     img_idx = img_list.index(cat_select)
     folder = './data/'
-    image_files = ['cat (1).jpg','cat (2).jpg','cat (3).jpg']
+    image_files = ['cat(1).jpg','cat(2).jpg','cat(3).jpg']
 
     image_path = os.path.dirname(os.path.abspath(__file__)) + folder + image_files[img_idx]
     image = Image.open(image_path)
